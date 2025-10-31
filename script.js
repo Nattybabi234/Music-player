096ee1a29cd61e429de39d91f3b5747e804bde18
Nathania Layeni <omoebun@yahoo.com>
2025-10-31 14:17:54 +0000

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const songs = [
     title: "E ti tobi to o",
     artist: "EmmaOMG",
     src: "EmmaOMG-E-Ti-Tobi-To.mp3",
-    cover: "EmmaOMG E-Ti-Tobi-To-image.jpg"
+    cover: "EmmaOMG-E-Ti-Tobi-To image.jpg"
   }
 ];
 
@@ -116,5 +116,6 @@ volumeSlider.addEventListener('input', (e) => {
 });
 
 audio.volume = volumeSlider.value; // sets volume to 0.9 by default
+
 
 
